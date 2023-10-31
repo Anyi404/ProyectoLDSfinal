444960188b761bcd47203cd5b16204f6bcd3a0b2
Anyi404 <128089461+Anyi404@users.noreply.github.com>
2023-10-31 10:32:50 -0500

    Delete conectar.py

--- conectar.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-conexion = mysql.connector.connect(user='root', password='3306',
-                                    host='localhost',
-                                    database='lds_c',
-                                    port='3306')
-print(conexion)
-
